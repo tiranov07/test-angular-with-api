4dfd3bc2d9d587c6630b72e43db2ae587b5cc16a
tav <tav@hlcompany.ru>
2020-05-29 12:47:13 +0400

    + повторные запросы к api при ошибке, обработка ошибок запросов

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams, HttpErrorResponse } from "@angular/common/http";
 import { Config } from "../shared/config";
 import { Dictionary } from "../shared/models/Dictionary";
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class HttpService { 
@@ -13,7 +14,10 @@ export class HttpService {
 
     public GetRequest(url: string, urlParams: Dictionary<string,string>, headers?: Dictionary<string, string>) {
         let Options = this.GetOptions(headers, urlParams);
-        let res: Observable<any> = this.http.get(this.Server + url, Options);
+        let res: Observable<any> = this.http.get(this.Server + url, Options).pipe(
+            retry(3),
+            catchError(this.handleError)
+        );
         return res
     }
 
@@ -44,5 +48,21 @@ export class HttpService {
             return undefined
         }
     }
+
+    private handleError(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.error('An error occurred:', error.error.message);
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            console.error(
+                `Backend returned code ${error.status}, ` +
+                `body was: ${error.error}`
+            );
+        }
+        // return an observable with a user-facing error message
+        return throwError('Something bad happened; please try again later.');
+    };
 
 }
